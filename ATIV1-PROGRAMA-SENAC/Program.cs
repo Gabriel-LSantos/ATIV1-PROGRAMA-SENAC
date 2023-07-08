4a65179408168206c9fda9f85fc091649607930e
gabri <gabri@DESKTOP-I4U4N50>
2023-07-07 21:13:31 -0300

    Adicionar arquivos de projeto.

---
+++ ATIV1-PROGRAMA-SENAC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Qual é o seu nome?");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Qual é a sua idade?");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Em qual bairro você mora?");
+        string bairro = Console.ReadLine();
+
+        string nomeMaiusculo = nome.ToUpper();
+
+        Console.WriteLine($"Nome: {nomeMaiusculo}");
+        Console.WriteLine($"Idade: {idade}");
+        Console.WriteLine($"Bairro: {bairro}");
+    }
+}
